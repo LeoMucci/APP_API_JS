e172e975f8431289f4f7b1c3e8bebec89a034d58
LeoMucci <leocapramucci@gmail.com>
2023-08-18 16:25:58 -0300

    Aula18/08

---
+++ rotas/rotaproduto.js
@@ -0,0 +1,18 @@
+const roteador = require("express").Router();
+
+const produtos = [
+    {
+        "nome": "Aifone",
+        "fabricante": "Pera",
+        "valor": 9999.99
+    }
+
+]
+
+roteador.use("/",(requisicao, resposta)=>{
+    resposta.send(
+        JSON.stringify(produtos)
+    )
+})
+
+module.exports = roteador;
